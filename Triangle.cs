b110ea5c1bf7bc4fbe548242c889682f1c0c0eb7
Hidden <137702933+Hiddensquid03@users.noreply.github.com>
2024-09-16 12:22:37 +0100

    Triangle
    Added triangle

---
+++ Triangle.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OopShapes
+{
+    internal class Triangle : Shape2D, IHasArea
+    {
+        public Triangle(double width, double height) : base(width, height)
+        {
+        }
+
+
+
+        public double Area()
+        {
+            return _width * _height / 2;
+        }
+
+    }
+}
